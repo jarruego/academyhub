cecf74ccdfd593d078234e943d830473fb419066
jarruego <jarruego@gmail.com>
2025-01-15 09:19:57 +0100

    Actualizar esquemas de base de datos para las tablas de centros y empresas, cambiando tipos de datos y ajustando restricciones

--- server/src/database/schema/tables/center.table.ts
+++ server/src/database/schema/tables/center.table.ts
@@ -1,15 +1,15 @@
 
-import { pgTable, serial, text, bigint } from "drizzle-orm/pg-core";
+import { pgTable, serial, varchar, integer } from "drizzle-orm/pg-core";
 import { TIMESTAMPS } from "./timestamps";
 import { companyTable } from "./company.table";
 
 export const centerTable = pgTable('centers', {
     id_center: serial().primaryKey(),
-    center_name: text().notNull(),
-    employer_number: text().notNull(),
-    id_company: bigint({ mode: 'number' }).references(() => companyTable.id_company).notNull(),
-    contact_person: text().notNull(),
-    contact_phone: text().notNull(),
-    contact_email: text().notNull(),
+    center_name: varchar({length: 128}).notNull(),
+    employer_number: varchar({length: 128}).notNull(),
+    id_company: integer().notNull().references(() => companyTable.id_company),
+    contact_person: varchar({length: 64}).notNull(),
+    contact_phone: varchar({length: 32}).notNull(),
+    contact_email: varchar({length: 128}).notNull(),
     ...TIMESTAMPS,
 });
--- server/src/database/schema/tables/company.table.ts
+++ server/src/database/schema/tables/company.table.ts
@@ -1,8 +1,8 @@
-import { pgTable, bigserial, varchar } from "drizzle-orm/pg-core";
+import { pgTable, varchar, serial } from "drizzle-orm/pg-core";
 import { TIMESTAMPS } from "./timestamps";
 
 export const companyTable = pgTable('companies', {
-    id_company: bigserial({ mode: 'number' }).primaryKey(),
+    id_company: serial().primaryKey(),
     company_name: varchar({length: 128}).notNull(),
     corporate_name: varchar({length: 256}).notNull(),
     cif: varchar({length: 12}).notNull().unique(),
