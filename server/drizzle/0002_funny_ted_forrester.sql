7f1cd6495201aadc17ad1b56357a881b9e95f196
jarruego <jarruego@gmail.com>
2025-01-15 09:30:25 +0100

    Agregar tabla de centros y actualizar esquema de base de datos para empresas

---
+++ server/drizzle/0002_funny_ted_forrester.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "companies" ALTER COLUMN "company_name" SET DATA TYPE varchar(128);--> statement-breakpoint
+ALTER TABLE "companies" ALTER COLUMN "corporate_name" SET DATA TYPE varchar(256);--> statement-breakpoint
+ALTER TABLE "companies" ALTER COLUMN "cif" SET DATA TYPE varchar(12);
--- server/src/database/schema.ts
+++ server/src/database/schema.ts
@@ -1,5 +1,7 @@
 import { companyTable } from "./schema/tables/company.table";
+import { centerTable } from "./schema/tables/center.table";
 import { userTable } from "./schema/tables/user.table";
 
 export const users = userTable;
 export const companies = companyTable;
+export const centers = centerTable;
