421a909c5cba1eb11324c04881d9bbb40c6eae04
jarruego <jarruego@gmail.com>
2025-01-20 11:05:59 +0100

    Modificar columnas en la tabla de grupos para permitir valores nulos en moodle_id, description, start_date y end_date; actualizar el registro de versiones en el diario

---
+++ server/drizzle/0002_complex_moon_knight.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "groups" ALTER COLUMN "moodle_id" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "groups" ALTER COLUMN "description" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "groups" ALTER COLUMN "start_date" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "groups" ALTER COLUMN "end_date" DROP NOT NULL;
