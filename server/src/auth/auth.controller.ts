b96ca435fe568afe9f33a71815b779c77b6eb3ca
Jose Alberto Arruego <jarruego@gmail.com>
2025-01-27 17:55:54 +0100

    Agregar documentación y habilitar el endpoint de registro de usuarios en AuthController

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -6,9 +6,22 @@ import { LoginDTO } from "src/dto/auth/login.dto";
 import { CreateUserDTO } from "src/dto/auth/create-user.dto";
 
 @Controller("auth")
+/**
+ * Controller responsible for handling authentication-related requests.
+ */
 export class AuthController {
+  /**
+   * Constructs an instance of AuthController.
+   * @param authService - The authentication service used to handle authentication logic.
+   */
   constructor(private readonly authService: AuthService) {}
 
+  /**
+   * Handles user login requests.
+   * 
+   * @param loginDto - Data Transfer Object containing login credentials.
+   * @returns A promise that resolves with the authentication result.
+   */
   @Throttle({
     default: { limit: 8, ttl: 60 * 1000 },
   })
@@ -19,10 +32,15 @@ export class AuthController {
     return this.authService.signIn(loginDto);
   }
 
-  // @Public()
-  // @HttpCode(HttpStatus.CREATED)
-  // @Post("signup")
-  // async signup(@Body() createUserDto: CreateUserDTO) {
-  //   return this.authService.signUp(createUserDto);
-  // }
+  /**
+   * Handles user signup requests.
+   * 
+   * @param createUserDto - Data Transfer Object containing user registration details.
+   * @returns A promise that resolves with the registration result.
+   */
+  @HttpCode(HttpStatus.CREATED)
+  @Post("signup")
+  async signup(@Body() createUserDto: CreateUserDTO) {
+    return this.authService.signUp(createUserDto);
+  }
 }
