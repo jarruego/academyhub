5c6c77c6a089a7ca4716433863edcc07a83cfdf6
jarruego <jarruego@gmail.com>
2025-12-18 10:25:32 +0100

    feat(normalize-search): update regex to preserve special characters for email in normalization

--- client/src/utils/normalize-search.ts
+++ client/src/utils/normalize-search.ts
@@ -6,6 +6,6 @@ export function normalizeSearch(input?: string): string {
     .normalize('NFD')
     .replace(/[\u0300-\u036f]/g, '') // remove diacritics
     .replace(/_/g, ' ') // treat underscores as spaces
-    .replace(/[^\w\s]/g, '') // remove special characters
+    .replace(/[^\w\s@.-]/g, '') // remove special characters but preserve @ . - for email
     .toLowerCase();
 }
